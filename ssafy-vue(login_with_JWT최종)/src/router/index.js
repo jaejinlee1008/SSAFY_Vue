236e519d506b3d57f48d13d28ca7abcb7066d50e
jaejin <ghkfuddks@naver.com>
2023-11-17 16:06:10 +0900

    23/11/17_jwt로그인 최종

---
+++ ssafy-vue(login_with_JWT최종)/src/router/index.js
@@ -0,0 +1,111 @@
+import { createRouter, createWebHistory } from "vue-router";
+import TheMainView from "../views/TheMainView.vue";
+import TheElectricChargingStationView from "@/views/TheElectricChargingStationView.vue";
+// import TheBoardView from "../views/TheBoardView.vue";
+
+import { storeToRefs } from "pinia";
+import { useMemberStore } from "@/stores/member";
+
+const onlyAuthUser = async (to, from, next) => {
+  const memberStore = useMemberStore();
+  const { userInfo, isValidToken } = storeToRefs(memberStore);
+  const { getUserInfo } = memberStore;
+
+  let token = sessionStorage.getItem("accessToken");
+
+  if (userInfo.value != null && token) {
+    await getUserInfo(token);
+  }
+  if (!isValidToken.value || userInfo.value === null) {
+    next({ name: "user-login" });
+  } else {
+    next();
+  }
+};
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      name: "main",
+      component: TheMainView,
+    },
+    {
+      path: "/user",
+      name: "user",
+      component: () => import("@/views/TheUserView.vue"),
+      children: [
+        {
+          path: "login",
+          name: "user-login",
+          component: () => import("@/components/users/UserLogin.vue"),
+        },
+        {
+          path: "join",
+          name: "user-join",
+          component: () => import("@/components/users/UserRegister.vue"),
+        },
+        {
+          path: "mypage",
+          name: "user-mypage",
+          beforeEnter: onlyAuthUser,
+          component: () => import("@/components/users/UserMyPage.vue"),
+        },
+        // {
+        //   path: "modify/:userid",
+        //   name: "user-modify",
+        //   component: () => import("@/components/users/UserModify.vue"),
+        // },
+      ],
+    },
+    {
+      path: "/board",
+      name: "board",
+      // component: TheBoardView,
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import("../views/TheBoardView.vue"),
+      redirect: { name: "article-list" },
+      children: [
+        {
+          path: "list",
+          name: "article-list",
+          component: () => import("@/components/boards/BoardList.vue"),
+        },
+        {
+          path: "view/:articleno",
+          name: "article-view",
+          beforeEnter: onlyAuthUser,
+          component: () => import("@/components/boards/BoardDetail.vue"),
+        },
+        {
+          path: "write",
+          name: "article-write",
+          beforeEnter: onlyAuthUser,
+          component: () => import("@/components/boards/BoardWrite.vue"),
+        },
+        {
+          path: "modify/:articleno",
+          name: "article-modify",
+          beforeEnter: onlyAuthUser,
+          component: () => import("@/components/boards/BoardModify.vue"),
+        },
+      ],
+    },
+    {
+      path: "/estations",
+      name: "estations",
+      component: TheElectricChargingStationView,
+    },
+    {
+      path: "/todos",
+      name: "todos",
+      beforeEnter: onlyAuthUser,
+      component: () => import("@/views/TheTodoView.vue"),
+    },
+  ],
+});
+
+export default router;
